d3a3d9d1600a43bf2d1adf4cca5aaf3e9a4f75fc
Fabian Rapp <fabian.rapp@edu.campus02.at>
2015-10-10 15:08:48 +0200

    Erste LV

---
+++ src/ErsteLV.java
@@ -0,0 +1,18 @@
+
+public class ErsteLV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean treeFront = true;
+		int numberLeaves =0;
+		char meinZeichen = 'X';
+		
+		System.out.println("Ist ein Baum voraus? " +treeFront);
+		System.out.println();
+		System.out.println("Anzahl der Blätter: " +meinZeichen);
+		System.out.println("Anzahl der Blätter: " +numberLeaves);
+		
+	}
+
+}
